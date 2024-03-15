5ed68f98ad97bb61412ea57c7b12977ca0686be6
AnnalingamGowsigan <e18115@eng.pdn.ac.lk>
2024-03-15 14:31:18 +0530

    Add api gateway.

---
+++ src/main/java/com/spring/springbootmicroservicesapigateway/SpringBootMicroservicesApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springbootmicroservicesapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springbootmicroservicesapigateway/SpringBootMicroservicesApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootmicroservicesapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
